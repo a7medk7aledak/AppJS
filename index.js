57ee1258d02ca3c68aa1594fe4c4ccb4d5d15c7b
Ahmed Khaled <92623979+a7medk7aledak@users.noreply.github.com>
2024-06-07 10:36:08 +0300

    .

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("jdaskks")
+function getPosts() {
+    let request = new XMLHttpRequest()
+    request.open("GET","https://jsonplaceholder.typicode.com/posts");
+    request.responseType = "json";
+    request.send();
+    request.onload = function () {
+        if (request.status >= 200 && request.status < 300) {
+            let posts = request.response;
+            for (post of posts) {
+                document.getElementById("content").innerHTML += `<h1>${post.title}</h1>`
+            }
+        } else {
+                alert("error")
+        }
+    }
+}
