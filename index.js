ef52fc91a1069aa57602d1b84b8444ae3b2d21a4
Ahmed Khaled <92623979+a7medk7aledak@users.noreply.github.com>
2024-06-07 11:10:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ function getPosts() {
     request.onload = function () {
         if (request.status >= 200 && request.status < 300) {
             let posts = request.response;
-            document.getElementById("posts").innerHTML = cont
             for (post of posts) {
                 let cont =`<div class = "data">
                     <h3>${post.title}</h3>
