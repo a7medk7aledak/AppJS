286ac0fd2421a4c80e12f2976c529da8fb4d9c2b
Ahmed Khaled <92623979+a7medk7aledak@users.noreply.github.com>
2024-06-07 14:37:36 +0300

    .

---
+++ scripts.js
@@ -0,0 +1,54 @@
+document.addEventListener("DOMContentLoaded", function () {
+  fetchUsers();
+});
+
+function fetchUsers() {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://jsonplaceholder.typicode.com/users", true);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      var users = JSON.parse(xhr.responseText);
+      populateUsers(users);
+    }
+  };
+  xhr.send();
+}
+
+function populateUsers(users) {
+  const contactList = document.getElementById("contact-list");
+  users.forEach((user) => {
+    const contact = document.createElement("div");
+    contact.classList.add("contact");
+    contact.setAttribute("data-user-id", user.id);
+    contact.innerHTML = `<p class="name">${user.name}</p><p class="email">${user.email}</p>`;
+    contact.addEventListener("click", () => showUserPosts(user.id));
+    contactList.appendChild(contact);
+  });
+}
+
+function showUserPosts(userId) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(
+    "GET",
+    `https://jsonplaceholder.typicode.com/posts?userId=${userId}`,
+    true
+  );
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      var posts = JSON.parse(xhr.responseText);
+      displayPosts(posts);
+    }
+  };
+  xhr.send();
+}
+
+function displayPosts(posts) {
+  const messageList = document.getElementById("message-list");
+  messageList.innerHTML = ""; // Clear previous messages
+  posts.forEach((post) => {
+    const message = document.createElement("div");
+    message.classList.add("message", "active");
+    message.innerHTML = `<h3>${post.title}</h3><p>${post.body}</p>`;
+    messageList.appendChild(message);
+  });
+}
